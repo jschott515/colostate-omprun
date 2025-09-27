700941b76255a79572a1b7ea63f94224231c4f54
Jordan Schott <C837986388@shark.cs.colostate.edu>
2025-09-26 19:40:28 -0600

    Create info flag, better taskset default

--- README.md
+++ README.md
@@ -17,9 +17,25 @@ Runs a series of trials using the provided test command. Has command line option
 * Taskset mask
 * Regular expression to parse result time from command output
 * Output directory for results .csv files
+* Optional brief description of test
 
 ### Basic Usage
-- `omprun "<Test Command>"`
+- `omprun "./bin/PolyMultDCK.time-par 65535 128" --threads 1:8 --info "Testing my program!"`
+
+Produces `omprun_09262025_194521.csv`
+```
+# Command: `./bin/PolyMultDCK.time-par 65535 128`
+# Brief: Testing my program!
+Threads,Time
+1,0.02547157142857143
+2,0.015355571428571428
+3,0.011438857142857141
+4,0.009935285714285714
+5,0.00871457142857143
+6,0.00854542857142857
+7,0.008761
+8,0.009985142857142857
+```
 
 For help:
 - `omprun --help`
--- omprun.py
+++ omprun.py
@@ -12,13 +12,8 @@ import typing
 
 DEFAULT_THREADS = (1, 8)
 DEFAULT_REPS = 7
-DEFAULT_MASK = 0xFFFFFFFF
 DEFAULT_REGEX = r'\b\d+\.\d+|\b\d+|\B\.\d+'
 DEFAULT_OUT = 'results'
-
-
-def sanitize_filename(filename: str) -> str:
-    return re.sub(r'[\.<>:"/\\|?*]', '', filename.replace(' ', '_'))
 
 
 def get_thread_range(arg: typing.Sequence[int], default_base: int = 1) -> typing.Tuple[int, int]:
@@ -29,10 +24,22 @@ def get_thread_range(arg: typing.Sequence[int], default_base: int = 1) -> typing
     return start, stop
 
 
+def get_default_mask(threads: int) -> int:
+    if threads <= 8:
+        return 0x00005555
+    if threads <= 16:
+        return 0x0000FFFF
+    return 0xFFFFFFFF
+
+
 def main() -> int:
     cfg = parse_args()
     os.makedirs(cfg.out, exist_ok=True)
-    with open(f"{cfg.out}/{sanitize_filename(cfg.TEST)}_{datetime.datetime.now().strftime('%m%d%Y_%H%M%S')}.csv", "w") as f:
+    with open(f"{cfg.out}/omprun_{datetime.datetime.now().strftime('%m%d%Y_%H%M%S')}.csv", "w") as f:
+        # Write Header
+        f.write(f"# Command: `{cfg.TEST}`\n")
+        if cfg.info is not None:
+            f.write(f"# Brief: {cfg.info}\n")
         writer = csv.writer(f)
         writer.writerow(["Threads", "Time"])
 
@@ -42,9 +49,11 @@ def main() -> int:
             for rep in range(cfg.reps):
                 # Run the test
                 print(f"Threads {thread}, Rep {rep}")
-                cmd = f"OMP_NUM_THREADS={thread} taskset {hex(cfg.mask)} " + cfg.TEST
+                mask = get_default_mask(thread) if cfg.mask is None else cfg.mask
+                cmd = f"OMP_NUM_THREADS={thread} taskset {hex(mask)} " + cfg.TEST
                 test = subprocess.run(cmd, shell=True, capture_output=True)
-                assert test.returncode == 0, f"`{cmd}` exited with {test.returncode}"
+                if test.returncode != 0:
+                    raise ValueError(f"`{cmd}` exited with {test.returncode}")
                 result = test.stdout.decode()
                 print(result.strip())
                 # Pull the result from stdout
@@ -65,12 +74,14 @@ def parse_args() -> argparse.Namespace:
                         help="Number of threads to test, [start:]stop (i.e. `5` runs 1-5 threads, `2:3` runs 2-3 threads)")
     parser.add_argument("--reps", type=int, default=DEFAULT_REPS,
                         help="Number of reps to run per test")
-    parser.add_argument("--mask", type=lambda x: int(x, 0), default=DEFAULT_MASK,
-                        help="Taskset Mask (i.e. 0x0000FFFF)")
+    parser.add_argument("--mask", type=lambda x: int(x, 0), default=None,
+                        help="Taskset Mask. Uses the ideal mask if not specified (i.e. 0x5555 for 8 threads, 0xFFFF for 16 threads, etc...)")
     parser.add_argument("--regex", type=str, default=DEFAULT_REGEX,
                         help="Regular expression to parse stdout of the TEST.")
     parser.add_argument("--out", "-o", type=str, default=DEFAULT_OUT,
                         help="Output directory for results.")
+    parser.add_argument("--info", "-i", type=str, default=None,
+                        help="Optional note to include in output file.")
     cfg = parser.parse_args()
     cfg.threads = get_thread_range(cfg.threads)
     return cfg
